3dffb3979fef76f76c0fce7a2ac5031fbc40a3e3
piyushpandey999 <thenotoriousbalor@gmail.com>
2024-08-26 12:18:59 +0530

    commit changes in second largest number in array

--- src/CountNumbersInaRangeInArray.java
+++ src/CountNumbersInaRangeInArray.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class CountNumbersInaRangeInArray {
 
     public static void main(String[] args) {
-        int r1=1,r2=5,count=0;
-        int[] arr = {1,2,3,5,6,0,4};
+        int r1 = 1, r2 = 5, count = 0;
+        int[] arr = { 1, 2, 3, 5, 6, 0, 4 };
         for (int j : arr) {
             if (j >= r1 && j <= r2) {
                 count++;
--- src/SecondLargestNumberinArray2.java
+++ src/SecondLargestNumberinArray2.java
@@ -5,12 +5,20 @@ import java.util.Collections;
 public class SecondLargestNumberinArray2 {
     public static void main(String[] args) {
        Integer[] array = {5,5,6,3,7,8,3,1};
-        Arrays.sort(array, Collections.reverseOrder());
-        for (int i=1;i<array.length;i++) {
-            if (array[i] != array[0]){
-                System.out.println("second largest number in array is " + array[i]);
-                return;
-            }
-        }
+       int[] arr = {3,44,4,8,3,9,10};
+//        Arrays.sort(array, Collections.reverseOrder());
+//        for (int i=1;i<array.length;i++) {
+//            if (array[i] != array[0]){
+//                System.out.println("second largest number in array is " + array[i]);
+//                return;
+//            }
+//        }
+
+        System.out.println("second largest number in array is " + getSecondLargest(arr,arr.length));
+    }
+
+    public static int getSecondLargest(int[] arr , int length) {
+        Arrays.sort(arr);
+        return arr[length - 2];
     }
 }
