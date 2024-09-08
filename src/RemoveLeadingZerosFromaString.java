6e304655024131c3420fd7b5b050df1ff3fb4c59
piyushpandey999 <thenotoriousbalor@gmail.com>
2024-09-08 22:31:20 +0530

    added method2 to remove leading zeros from a string

--- src/RemoveLeadingZerosFromaString.java
+++ src/RemoveLeadingZerosFromaString.java
@@ -8,7 +8,8 @@ public class RemoveLeadingZerosFromaString {
         String str = scanner.nextLine();
         // String str = "0000123405";
         System.out.println("string before removing zeros -" + str);
-        System.out.println("string after removing zeros -" + method1(str));
+//        System.out.println("string after removing zeros by method 1 -" + method1(str));
+        System.out.println("String after removing zeros by method 2 -" + method2(str));
     }
 
     public static String method1(String str) {
@@ -21,6 +22,13 @@ public class RemoveLeadingZerosFromaString {
         // below replace function removes i characters from given index ( 0 here)
         sb.replace(0, i, "");
 
+        return sb.toString();
+    }
+
+    public static String method2(String str){
+        StringBuilder sb = new StringBuilder(str);
+        while (sb.length() >0 && sb.charAt(0) == '0')
+            sb.deleteCharAt(0);
         return sb.toString();
     }
 
--- src/Test3.java
+++ src/Test3.java
@@ -1,27 +1,48 @@
 public class Test3 {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12};
-        System.out.println(secondLargest(array));
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        String str = "00000000987";
+//        System.out.println(secondLargest(array));
+        System.out.println(removeleadingzeros2(str));
     }
-public static int secondLargest(int[] input){
+
+    public static int secondLargest(int[] input) {
         int firstLargest, secondLargest;
-        if(input[0]>input[1]){
-            firstLargest=input[0];
-            secondLargest=input[1];
+        if (input[0] > input[1]) {
+            firstLargest = input[0];
+            secondLargest = input[1];
+        } else {
+            secondLargest = input[0];
+            firstLargest = input[1];
         }
-        else {
-            secondLargest=input[0];
-            firstLargest=input[1];
-        }
-        for (int i=2;i< input.length;i++) {
-            if (input[i]>firstLargest){
-                secondLargest=firstLargest;
-                firstLargest=input[i];
-            } else if (input[i]<firstLargest && input[i]>secondLargest) {
-                secondLargest=input[i];
+        for (int i = 2; i < input.length; i++) {
+            if (input[i] > firstLargest) {
+                secondLargest = firstLargest;
+                firstLargest = input[i];
+            } else if (input[i] < firstLargest && input[i] > secondLargest) {
+                secondLargest = input[i];
             }
         }
-     return secondLargest;
-}
+        return secondLargest;
+    }
 
+    public static String removeleadingzeros(String str) {
+        int i = 0;
+        while (i < str.length() && str.charAt(i) == '0')
+            i++;
+        System.out.println(i);
+        StringBuilder sb = new StringBuilder(str);
+        sb.replace(0, i, "");
+
+        return sb.toString();
+    }
+
+    public static String removeleadingzeros2(String str) {
+        int i = 0;
+        StringBuilder sb = new StringBuilder(str);
+        while (sb.length() >0 && sb.charAt(0) =='0')
+            sb.deleteCharAt(0);
+
+        return sb.toString();
+    }
 }
