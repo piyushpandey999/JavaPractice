4662d3c1dfe1b0f0ef1605645b8885e06b9bf4f1
piyushpandey999 <thenotoriousbalor@gmail.com>
2024-10-30 11:56:55 +0530

    Adding program to sort an array

---
+++ src/SortAnArray.java
@@ -0,0 +1,27 @@
+public class SortAnArray {
+
+    public static void sortArray() {
+        int[] arr = { 5, 2, 8, 1, 9 };
+        int len = arr.length;
+
+        int temp = 0;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int k = 0; k < len; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        sortArray();
+    }
+}
