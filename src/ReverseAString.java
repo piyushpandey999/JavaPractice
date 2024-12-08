ca4ad155f095e22abb3bea35bf8fcbba0857fa0a
piyushpandey999 <thenotoriousbalor@gmail.com>
2024-12-08 13:26:30 +0530

    no changes

--- src/ReverseAString.java
+++ src/ReverseAString.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class ReverseAString {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("enter the string");
-        String str = scanner.nextLine();
-        // String str = "ram";
-        char[] chararray = str.toCharArray();
-        //
-        for (int i = chararray.length - 1; i >= 0; i--) {
-            System.out.print(chararray[i]);
-        }
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("enter the string");
+         String str = scanner.nextLine();
+         // String str = "ram";
+         char[] chararray = str.toCharArray();
+         //
+         for (int i = chararray.length - 1; i >= 0; i--) {
+         System.out.print(chararray[i]);
+         }
     }
 }
