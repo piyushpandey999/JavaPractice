1f9504181810b0824bc87e75601f8789bdb39528
piyushpandey999 <thenotoriousbalor@gmail.com>
2024-12-08 13:22:25 +0530

    adding functions to sort array in descending and ascending order

--- src/SortAnArray.java
+++ src/SortAnArray.java
@@ -1,6 +1,6 @@
 public class SortAnArray {
 
-    public static void sortArray() {
+    public static void sortArrayinAscendingOrder() {
         int[] arr = { 5, 2, 8, 1, 9 };
         int len = arr.length;
 
@@ -20,8 +20,31 @@ public class SortAnArray {
             System.out.print(arr[k] + " ");
         }
     }
+    public static void sortArrayinDescendingOrder() {
+        int[] arr = { 5, 2, 8, 1, 9 };
+        int len = arr.length;
+
+        int temp = 0;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int k = 0; k < len; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
 
     public static void main(String[] args) {
-        sortArray();
+        System.out.println("Array in ascending order");
+        sortArrayinAscendingOrder();
+        System.out.println("Array in descending order");
+        sortArrayinDescendingOrder();
     }
 }
