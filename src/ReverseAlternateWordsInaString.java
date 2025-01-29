cc3d580ff6042fc3f3cf238e83db7e8df8729d77
piyushpandey999 <thenotoriousbalor@gmail.com>
2025-01-30 00:28:24 +0530

    Adding program to reverse alternate words in a string

--- src/ReverseAlternateWordsInaString.java
+++ src/ReverseAlternateWordsInaString.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ReverseAlternateWordsInaString {
+public class ReverseAlternateWordsInaString {
+
+    public static void main(String[] args) {
+        String str = "ading this program in github repo";
+        System.out.println(revsre(str));
+    }
+
+    public static String revsre(String str) {
+        String[] words = str.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (i%2!=0) {
+                words[i] = new StringBuilder(words[i]).reverse().toString();
+            }
+        }
+        return String.join(" ",words);
+    }
 }
+
