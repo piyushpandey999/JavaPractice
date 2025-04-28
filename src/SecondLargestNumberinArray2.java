c49c6e6736ee9f0e950b7e5101d02e1abd187df1
piyushpandey999 <thenotoriousbalor@gmail.com>
2025-04-28 23:04:37 +0530

    Adding new method for finding second largest number in array

--- src/SecondLargestNumberinArray2.java
+++ src/SecondLargestNumberinArray2.java
@@ -13,11 +13,32 @@ public class SecondLargestNumberinArray2 {
         // }
         // }
 
-        System.out.println("second largest number in array is " + getSecondLargest(arr, arr.length));
+//        System.out.println("second largest number in array is " + getSecondLargest(arr, arr.length));
+        SecondLargest(arr);
     }
 
+    // Method to get second largest in an array using sort method
     public static int getSecondLargest(int[] arr, int length) {
         Arrays.sort(arr);
         return arr[length - 2];
+    }
+
+
+    // Another method to find second largest number in an array without using any inbuilt functions -----------
+    public static void SecondLargest(int[] arr){
+        int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+        System.out.println("largest number is "+ largest);
+
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]>secondLargest && arr[i]<largest){
+                secondLargest = arr[i];
+            }
+        }
+        System.out.println("second largest number is "+ secondLargest);
     }
 }
