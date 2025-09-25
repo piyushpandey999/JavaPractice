9d0db045135d57fb715767c6984602b8cc333747
piyushpandey999 <thenotoriousbalor@gmail.com>
2025-09-25 19:47:46 +0530

    Adding program to reverse string with spaces in place

--- src/ReveseStringWithSpacesInPlace.java
+++ src/ReveseStringWithSpacesInPlace.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ReveseStringWithSpacesInPlace {
+
+
+public class ReveseStringWithSpacesInPlace {
+
+    // ------ Java program to reverse a string with spaces in place ------------
+
+    public static void main(String[] args) {
+        String s = "this is not";
+        reverseStringWithSpaceInPlace(s);
+    }
+
+
+    public static void reverseStringWithSpaceInPlace(String input){
+        StringBuilder sb = new StringBuilder(input.replace(" ",""));
+        sb.reverse();
+
+        for (int i=0;i<input.length();i++){
+            if (input.charAt(i)==' '){
+                sb.insert(i," ");
+            }
+        }
+        System.out.println(sb);
+    }
 }
